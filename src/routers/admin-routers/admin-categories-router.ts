be119579b30be66a67823f03b7bb675b27508d60
Endy-Guff <andreymaranchak@yandex.ru>
2024-07-18 22:39:32 +0300

    изменил название ручки создания категории

--- src/routers/admin-routers/admin-categories-router.ts
+++ src/routers/admin-routers/admin-categories-router.ts
@@ -4,4 +4,4 @@ import categoriesController from "../../controllers/categories-controller";
 
 export const adminCategoriesRouter: Router = express.Router()
 
-adminCategoriesRouter.post('/create', categoriesController.create)
+adminCategoriesRouter.post('/', categoriesController.create)
